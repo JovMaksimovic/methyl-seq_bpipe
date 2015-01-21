425c7cb2c61e73431c1a47e3cfa5747ac9ac0b45
JovMaksimovic <jvnmaksimovic@gmail.com>
2015-01-21 14:25:03 +1100

    Added comments to pipeline

--- methylseq.groovy
+++ methylseq.groovy
@@ -1,4 +1,8 @@
-REF = "/mnt/storage/shared/genomes/hg19/fasta/single_fasta_file/"
+// Bpipe for analysing bisulfite sequencing methyl-seq data
+// Written by: Jovana Maksimovic
+// Last modified on: 21/01/2015
+
+REF = "/mnt/storage/shared/genomes/hg19/fasta/single_fasta_file/" // Location of bisulfite converted genome
 
 def get_name(filename) {
 
@@ -10,7 +14,7 @@ def get_name(filename) {
 }
 
 sra_to_fastq = {
-  
+  // Convert SRA files to fastq format 
   transform(".sra") to ("_1.fastq.gz","_2.fastq.gz"){
     exec """
       fastq-dump --split-3 --gzip $input.sra;
@@ -19,7 +23,7 @@ sra_to_fastq = {
 }
 
 fastqc = {
-    doc "Run FASTQC to generate QC metrics for the reads"
+    // Run FASTQC to generate QC metrics for the reads  
     output.dir = "fastqc"
     transform(".fastq.gz",".fastq.gz") to ("_fastqc.zip","_fastqc.zip"){
         exec """
@@ -29,6 +33,8 @@ fastqc = {
 }
 
 trim_reads = {
+  // Trim "clip5" bp from 5' end of both read pairs
+  // Trim "clip3" bp from 3' end of both read pairs adapter/quality trimming has been performed
   clip5 = "10"
   clip3 = "5"
 
@@ -41,6 +47,7 @@ trim_reads = {
 }
 
 rename_fastq = {
+  // Rename fastq files; this helps next stage tu run correctly
   newname1 = get_name(input1)
   newname2 = get_name(input2)
 
@@ -53,6 +60,7 @@ rename_fastq = {
 }
 
 map_reads = {
+  // Map read pairs using bismark and bowtie2
   transform("gz_bismark_bt2_pe.bam") {
     exec """
       bismark --bowtie2 --bam -n 1 $REF -1 $input1.gz -2 $input2.gz
@@ -61,6 +69,7 @@ map_reads = {
 }
 
 dedupe = {
+  // Remov duplicates
   transform("bam") to ("deduplicated.bam"){
     exec """
       deduplicate_bismark --paired --bam $input.bam
@@ -69,6 +78,7 @@ dedupe = {
 }
 
 extract_methylation = {
+  // Get methylation calls at each CpG
   output.dir = "methylation_calls"
   transform("bam") to ("CpG_report.txt"){
     exec """
@@ -79,6 +89,7 @@ extract_methylation = {
 }
 
 get_covered_CpGs = {
+  // Create files containing only CpGs that are covered by 1+ reads
   filter("covered") {
     exec """
       awk '\$1 ~ /chr[12]?[0-9]\$/ && (\$4 + \$5 != 0) {printf(\"%s\\t%s\\t%s\\t%s\\t%s\\n\",\$1,\$2,\$3,\$4+\$5,\$4)}'
@@ -88,12 +99,14 @@ get_covered_CpGs = {
 }
 
 sort_bam = {
+  // Sort bam file
   filter("sorted") {
          exec "samtools sort $input.bam $output.prefix"
   }
 }
 
 index_bam = {
+  // Index bam file
     transform("bam") to ("bam.bai") {
         exec "samtools index $input.bam"
     }
@@ -101,6 +114,7 @@ index_bam = {
 }
 
 run {
+  // Run the methyl-seq pipeline
   "%.sra" * [sra_to_fastq + fastqc + trim_reads + rename_fastq + map_reads] +
   "%.bam" * [dedupe + [extract_methylation + get_covered_CpGs, sort_bam + index_bam]]
 }
